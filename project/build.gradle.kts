d5c0e77ff18509968d0b28a31027069ca055fbb9
alexdvn <alexdgames01@gmail.com>
2025-10-09 07:09:38 -0400

    Create build.gradle.kts

---
+++ project/build.gradle.kts
@@ -0,0 +1,6 @@
+// set the Android gradle plugin version 
+plugins {
+    id("com.android.application") version "8.6.0" apply false
+    id("com.android.library") version "8.6.0" apply false
+    id("org.jetbrains.kotlin.android") version "2.0.20" apply false
+}
